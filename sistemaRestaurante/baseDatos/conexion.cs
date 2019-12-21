43c69fea0e153fd8d83dcc88f6d4323b8fb84828
Brandon Alexis <brandonalexis1112@gmail.com>
2019-12-21 12:07:01 -0500

    Primer commit

---
+++ sistemaRestaurante/baseDatos/conexion.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace sistemaRestaurante.baseDatos
+{
+    class conexion
+    {
+        private String cadenaConexionBaseDeDatos;
+        public String obtenerCadenaDeConexion()
+        {
+            //aqui se encuentra la cadena de conexion  modique en segun el sql de su maquina
+            cadenaConexionBaseDeDatos = "Data Source=201-26;Initial Catalog=restaurante;Persist Security Info=True;User ID=sa;Password=123456";
+            return cadenaConexionBaseDeDatos;
+        }
+
+
+        ///////
+        private SqlConnection conexionBaseDatos;
+        public conexion()
+        {
+            conexionBaseDatos = new SqlConnection(obtenerCadenaDeConexion());
+        }
+
+          public SqlConnection openDataBase()
+        {
+            conexionBaseDatos.Open();
+            if (conexionBaseDatos == null)
+            {
+                conexionBaseDatos.Close();
+            }
+            return conexionBaseDatos;
+        }
+
+       
+        public void closeDatabase()
+        {
+            try
+            {
+                conexionBaseDatos.Close();
+            }
+            catch (Exception _ex)
+            {
+                String mnsaje = _ex.Message;
+            }
+        }
+
+
+
+    }
+}
